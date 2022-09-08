743037718ea3c5d1cdc353fab931c4d949921694
diegocostares <diegocostar@uc.cl>
2022-09-08 16:57:02 -0400

    feat: init files - ayudantia

---
+++ backend/app.js
@@ -0,0 +1,24 @@
+import Koa from "koa";
+import koaBody from "koa-body";
+import KoaLogger from "koa-logger";
+import router from "./routes.js";
+
+const PORT = 3000;
+
+const app = new Koa();
+
+// Logs requests from the server
+app.use(KoaLogger());
+
+// Parse Request Body
+app.use(koaBody());
+
+// app.use((ctx, next) => {
+//   ctx.body = "Hello 2048 DCC!";
+// })
+
+app.use(router.routes());
+
+app.listen(PORT, () => {
+    console.log(`Starting app in port ${PORT}`);
+});
