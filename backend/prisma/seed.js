e5007f14d6c096f429368c1ee4cb4eedcd9c1686
Agustín Covarrubias <agucova@uc.cl>
2022-10-16 22:11:10 -0300

    fix: achievement level on schema and race in seed

--- backend/prisma/seed.js
+++ backend/prisma/seed.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-await-in-loop */
+/* eslint-disable no-restricted-syntax */
 import { PrismaClient } from '@prisma/client';
 import fs from 'fs';
 
@@ -8,25 +10,23 @@ function loadMembers() {
   const members = JSON.parse(fs.readFileSync('data/members.json', 'utf-8'));
 
   // Load members
-  members.map(async (member) => {
-    await prisma.member.create({
-      data: {
-        username: member.username,
-        role: member.role,
-        joinedAt: new Date(),
-        profile: {
-          create: {
-            name: member.name,
-            title: member.title,
-            telegramUsername: member.telegramUsername,
-            email: `${member.username}@uc.cl`,
-            avatarURL: `https://avatars.githubusercontent.com/${member.username}?s=120`,
-            // This is a mock email
-          },
+  return members.map((member) => prisma.member.create({
+    data: {
+      username: member.username,
+      role: member.role,
+      joinedAt: new Date(),
+      profile: {
+        create: {
+          name: member.name,
+          title: member.title,
+          telegramUsername: member.telegramUsername,
+          email: `${member.username}@uc.cl`,
+          avatarURL: `https://avatars.githubusercontent.com/${member.username}?s=120`,
+          // This is a mock email
         },
       },
-    });
-  });
+    },
+  }));
 }
 
 function loadAchievements() {
@@ -36,23 +36,25 @@ function loadAchievements() {
   );
 
   // Load achievements
-  achievements.map(async (achievement) => {
-    await prisma.achievement.create({
-      data: {
-        name: achievement.name,
-        description: achievement.description,
-        imageURL: achievement.imageURL,
-        type: achievement.type,
-        level: achievement.level,
-        createdAt: new Date(),
-        creator: {
-          connect: {
-            username: achievement.creator,
-          },
+  return achievements.map((achievement) => prisma.achievement.create({
+    data: {
+      name: achievement.name,
+      description: achievement.description,
+      imageURL: achievement.imageURL,
+      type: achievement.type,
+      level: {
+        connect: {
+          name: achievement.level,
         },
       },
-    });
-  });
+      createdAt: new Date(),
+      creator: {
+        connect: {
+          username: achievement.creator,
+        },
+      },
+    },
+  }));
 }
 
 function loadAchievementsOnMembers() {
@@ -60,28 +62,26 @@ function loadAchievementsOnMembers() {
     fs.readFileSync('data/achievements_on_members.json', 'utf-8'),
   );
 
-  achievementsOnMembers.map(async (achievementOnMember) => {
-    await prisma.achievementsOnMembers.create({
-      data: {
-        obtainedAt: new Date(),
-        awardedBy: {
-          connect: {
-            username: achievementOnMember.awardedBy,
-          },
-        },
-        member: {
-          connect: {
-            username: achievementOnMember.member,
-          },
-        },
-        achievement: {
-          connect: {
-            name: achievementOnMember.achievement,
-          },
+  return achievementsOnMembers.map((achievementOnMember) => prisma.achievementsOnMembers.create({
+    data: {
+      obtainedAt: new Date(),
+      awardedBy: {
+        connect: {
+          username: achievementOnMember.awardedBy,
         },
       },
-    });
-  });
+      member: {
+        connect: {
+          username: achievementOnMember.member,
+        },
+      },
+      achievement: {
+        connect: {
+          name: achievementOnMember.achievement,
+        },
+      },
+    },
+  }));
 }
 
 function loadRequests() {
@@ -89,42 +89,44 @@ function loadRequests() {
     fs.readFileSync('data/requests.json', 'utf-8'),
   );
 
-  requests.map(async (request) => {
-    await prisma.requests.create({
-      data: {
-        openedAt: new Date(),
-        openedBy: {
-          connect: {
-            username: request.openedBy,
-          },
+  return requests.map((request) => prisma.request.create({
+    data: {
+      openedAt: new Date(),
+      openedBy: {
+        connect: {
+          username: request.openedBy,
         },
-        state: request.state,
-        description: request.description,
       },
-    });
-  });
+      state: request.state,
+      description: request.description,
+      achievement: {
+        connect: {
+          name: request.achievement,
+        },
+      },
+    },
+  }));
 }
 
-function loadAchievementLevels() {
+async function loadAchievementLevels() {
   const levels = JSON.parse(
     fs.readFileSync('data/achievement_levels.json', 'utf-8'),
   );
 
-  Object.keys(levels).forEach((key) => {
-    // TO AGU: Debe llamarse con await el siguiente create de prisma?
-    prisma.achievementLevel.create({
-      data: {
-        level: key,
-        points: levels[key],
-      },
-  });
+  return levels.map((level) => prisma.achievementLevel.create({
+    data: {
+      name: level.name,
+      points: level.points,
+    },
+  }));
 }
 
 async function main() {
-  loadMembers();
-  loadAchievements();
-  loadAchievementsOnMembers();
-  loadRequests();
+  await loadMembers();
+  await loadAchievementLevels();
+  await loadAchievements();
+  await loadAchievementsOnMembers();
+  await loadRequests();
 }
 
-main();
+await main();
