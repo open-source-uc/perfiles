0d45341d9a962af413a20236cb86d593f483dc75
Agustín Covarrubias <agucova@uc.cl>
2022-10-31 03:55:14 -0300

    feat: finish navbar

--- frontend/src/components/common/Header.jsx
+++ frontend/src/components/common/Header.jsx
@@ -1,10 +1,11 @@
 import * as React from 'react';
-import { Link } from 'react-router-dom';
-import { ReactComponent as Logo } from '../../assets/images/logo.svg';
+
 import Navbar from './Navbar';
 
 export default function Header() {
   return (
-    <Navbar />
+    <header>
+      <Navbar />
+    </header>
   );
 }
--- frontend/src/components/common/Navbar.jsx
+++ frontend/src/components/common/Navbar.jsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { Fragment } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import { Disclosure, Menu, Transition } from '@headlessui/react';
-import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
+import { Bars3Icon, XMarkIcon, UserCircleIcon } from '@heroicons/react/24/outline';
 import UserContext from '../../contexts/userContext';
 import SwitchDark from './SwitchDark';
 
@@ -83,73 +83,84 @@ export default function Navbar() {
                 <SwitchDark />
 
                 {/* Profile dropdown */}
-                <Menu as="div" className="relative ml-3">
-                  <div>
-                    <Menu.Button className="flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-                      <span className="sr-only">Abrir menú de usuario</span>
-                      <img
-                        className="h-10 w-10 rounded-full"
-                        src={user?.profile?.avatarURL}
-                        alt=""
-                      />
-                    </Menu.Button>
-                  </div>
-                  <Transition
-                    as={Fragment}
-                    enter="transition ease-out duration-100"
-                    enterFrom="transform opacity-0 scale-95"
-                    enterTo="transform opacity-100 scale-100"
-                    leave="transition ease-in duration-75"
-                    leaveFrom="transform opacity-100 scale-100"
-                    leaveTo="transform opacity-0 scale-95"
-                  >
-                    <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                      <Menu.Item>
-                        {({ active }) => (
-                          <Link
-                            to={`/perfil/${user?.username}`}
-                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-                          >
-                            👤 Tu perfil
-                          </Link>
-                        )}
-                      </Menu.Item>
-                      <Menu.Item>
-                        {({ active }) => (
-                          <Link
-                            to="/solicitudes/"
-                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-                          >
-                            📝 Solicitudes
-                          </Link>
-                        )}
-                      </Menu.Item>
-                      { isAdmin(user) && (
-                      <Menu.Item>
-                        {({ active }) => (
-                          <Link
-                            to="/admin/"
-                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-                          >
-                            👨‍💻 Admin
-                          </Link>
-                        )}
+                {user ? (
+                  <Menu as="div" className="relative ml-3">
+                    <div>
+                      <Menu.Button className="flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                        <span className="sr-only">Abrir menú de usuario</span>
+                        <img
+                          className="h-10 w-10 rounded-full"
+                          src={user?.profile?.avatarURL}
+                          alt=""
+                        />
+                      </Menu.Button>
+                    </div>
+                    <Transition
+                      as={Fragment}
+                      enter="transition ease-out duration-100"
+                      enterFrom="transform opacity-0 scale-95"
+                      enterTo="transform opacity-100 scale-100"
+                      leave="transition ease-in duration-75"
+                      leaveFrom="transform opacity-100 scale-100"
+                      leaveTo="transform opacity-0 scale-95"
+                    >
+                      <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                        <Menu.Item>
+                          {({ active }) => (
+                            <Link
+                              to={`/perfil/${user?.username}`}
+                              className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
+                            >
+                              👤 Tu perfil
+                            </Link>
+                          )}
+                        </Menu.Item>
+                        <Menu.Item>
+                          {({ active }) => (
+                            <Link
+                              to="/solicitudes/"
+                              className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
+                            >
+                              📝 Solicitudes
+                            </Link>
+                          )}
+                        </Menu.Item>
+                        { isAdmin(user) && (
+                        <Menu.Item>
+                          {({ active }) => (
+                            <Link
+                              to="/admin/"
+                              className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
+                            >
+                              👨‍💻 Admin
+                            </Link>
+                          )}
 
-                      </Menu.Item>
-                      )}
-                      <Menu.Item>
-                        {({ active }) => (
-                          <a
-                            href="/?logout=true"
-                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-                          >
-                            🚪 Cerrar sesión
-                          </a>
+                        </Menu.Item>
                         )}
-                      </Menu.Item>
-                    </Menu.Items>
-                  </Transition>
-                </Menu>
+                        <Menu.Item>
+                          {({ active }) => (
+                            <a
+                              href="/?logout=true"
+                              className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
+                            >
+                              🚪 Cerrar sesión
+                            </a>
+                          )}
+                        </Menu.Item>
+                      </Menu.Items>
+                    </Transition>
+                  </Menu>
+                )
+                  : (
+                    <a
+                      href="/api/auth/login"
+                      className="flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 mx-2"
+                      title="Iniciar sesión"
+                    >
+                      <UserCircleIcon className="h-10 w-10 rounded-full text-osuc-white-3 bg-osuc-black-1" />
+                    </a>
+                  )}
               </div>
             </div>
           </div>
