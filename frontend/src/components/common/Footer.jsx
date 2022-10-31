19a4ab16add17cfedfa5fd35bec75241ec6ffb98
Agustín Covarrubias <agucova@uc.cl>
2022-10-31 03:36:08 -0300

    fix: footer in dark mode, simplify switch

--- frontend/src/components/common/Footer.jsx
+++ frontend/src/components/common/Footer.jsx
@@ -3,34 +3,40 @@ import * as React from 'react';
 
 export default function Footer() {
   return (
-    <footer>
+    <footer className="bg-osuc-black-3 text-osuc-white-3 py-6 text-center relative bottom-0 w-full">
       {/* Código autogenerado por https://chooser-beta.creativecommons.org/ */}
       <p>
         <a
           // eslint-disable-next-line react/no-unknown-property
           property="dct:title"
           href="https://perfiles.osuc.dev/"
+          className="underline"
         >
           Members OSUC
 
         </a
         >
+        {' '}
         de
+        {' '}
         <a
         // eslint-disable-next-line react/no-unknown-property
           property="cc:attributionName"
           href="https://osuc.dev/"
+          className="underline"
         >
           Open Source UC
 
         </a
         >
+        { ' '}
         está licenciado bajo
+        { ' '}
         <a
           href="http://creativecommons.org/licenses/by-nc-sa/4.0/?ref=chooser-v1"
           target="_blank"
           rel="license noopener noreferrer"
-          style={{ display: 'inline-block' }}
+          className="underline"
         >
           CC BY-NC-SA 4.0
         </a>
--- frontend/src/components/common/SwitchDark.jsx
+++ frontend/src/components/common/SwitchDark.jsx
@@ -25,7 +25,7 @@ export default function SwitchDark() {
 
   return (
     <button type="button" className={`w-16 h-10 flex content-center rounded-full p-1 bg-osuc-black-1 dark:bg-osuc-navyblue mx-2 ${isDark ? 'justify-end' : 'justify-start'}`} onClick={() => setIsDark(!isDark)}>
-      <motion.div className="handle-dark" layout transition={spring} />
+      <motion.div className="handle-dark w-8 h-8 block bg-osuc-white-3 rounded-full shadow-sm shadow-black" layout transition={spring} />
     </button>
   );
 }
