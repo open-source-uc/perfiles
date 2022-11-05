9b6de32d2ae56d13c73459b97cdd6e941cfc4eee
diegocostares <diegocostar@uc.cl>
2022-11-05 02:42:31 -0300

    fix: view tree

--- frontend/src/components/common/SkillTree.jsx
+++ frontend/src/components/common/SkillTree.jsx
@@ -1,3 +1,7 @@
+/* eslint-disable no-return-assign */
+/* eslint-disable react/destructuring-assignment */
+/* eslint-disable react/jsx-props-no-spreading */
+/* eslint-disable react/state-in-constructor */
 import React from 'react';
 import Tree from 'react-d3-tree';
 
@@ -92,35 +96,73 @@ const myTreeData = [
   },
 ];
 
-function NodeLabel({ nodeData }) {
-  return (
-    <div className="myLabelComponentIncard">
-      <img className="chapita" src={nodeData.image} alt=" " />
-      <text>{nodeData.name}</text>
-    </div>
-  );
-}
+const renderForeignObjectNode = ({
+  nodeDatum,
+  toggleNode,
+  foreignObjectProps,
+}) => (
+  <g onClick={toggleNode}>
+    <foreignObject {...foreignObjectProps}>
+      <div className="myLabelComponentIncard">
+        <img className="chapita" src={nodeDatum.image} alt=" " />
+        <div>
+          {nodeDatum.name}
+        </div>
+      </div>
+    </foreignObject>
+  </g>
+);
 
-// .attr("stroke", "red");
+const nodeSize = { x: 150, y: 150 };
+const foreignObjectProps = {
+  width: nodeSize.x,
+  height: nodeSize.y - 40,
+  x: -50,
+  y: -50,
+};
 
-export default function SkillTree() {
-  return (
-    <div className="treeWrapper">
-      <Tree
-          // initialDepth={2}
-          // depthFactor={270}
-        pathFunc="step"
-        orientation="vertical"
-        allowForeignObjects
-        translate={{ x: 150, y: 100 }} // Donde se visualiza
-        separation={{ siblings: 0.6, nonSiblings: 0.7 }}
-        nodeSize={{ x: 175, y: 175 }}
-        nodeLabelComponent={{
-          render: <NodeLabel />,
-          foreignObjectWrapper: { x: -50, y: -50 },
-        }}
-        data={myTreeData}
-      />
-    </div>
-  );
+export default class SkillTree extends React.PureComponent {
+  state = {};
+
+  componentDidMount() {
+    const dimensions = this.treeContainer.getBoundingClientRect();
+    this.setState({
+      translate: {
+        x: dimensions.width / 2,
+        y: 70,
+      },
+      data: myTreeData,
+      depth: 2,
+    });
+  }
+
+  render() {
+    return (
+      <div className="treeWrapper" ref={(tc) => (this.treeContainer = tc)}>
+        {this.state.data && (
+          <Tree
+            shouldCollapseNeighborNodes
+            orientation="vertical"
+            pathFunc="step"
+            data={this.state.data}
+            translate={this.state.translate} // Donde se visualiza
+            initialDepth={this.state.depth}
+            onNodeClick={this.onNodeClick}
+            separation={{ siblings: 0.6, nonSiblings: 0.7 }}
+            nodeSize={{ x: 175, y: 175 }}
+            // Generamos los nodos del arbol
+            renderCustomNodeElement={(rd3tProps) => renderForeignObjectNode(
+              { ...rd3tProps, foreignObjectProps },
+            )}
+            zoom={0.7}
+            // Pasamos los Estilos (No todos estan implementados, pero se deja como base)
+            pathClassFunc={() => 'linkBase'}
+            rootNodeClassName="rootNode"
+            branchNodeClassName="branchNode"
+            leafNodeClassName="leafNode"
+          />
+        )}
+      </div>
+    );
+  }
 }
