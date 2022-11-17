d6d432ffa7f18d3d8563465f7a69b86149bd3498
diegocostares <diegocostar@uc.cl>
2022-11-17 01:53:22 -0300

    feat: modularización de modales y cartas

--- frontend/src/components/Proyectos.jsx
+++ frontend/src/components/Proyectos.jsx
@@ -1,60 +1,40 @@
 /* eslint-disable max-len */
 /* eslint-disable no-unused-vars */
-import React, { useEffect, useContext, useState } from 'react';
+import React, {
+  useEffect, useContext, useState, Fragment,
+} from 'react';
 import { Helmet } from 'react-helmet';
-import { Tab } from '@headlessui/react';
+import { Tab, Dialog, Transition } from '@headlessui/react';
+import { CreateModal, FormProyectos, FormIdeas } from './common/CreateModal';
+import CardsProyects from './common/CardsProyects';
 
-// TODO: BORRAR luego de mergear a develop e importar la funcion desde utils
+// TODO: BORRAR luego de mergear a develop e importar la misma funcion desde utils
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ');
 }
 
-function Cards({ listElements }) {
+function BtnCreate({ onClick }) {
   return (
-    <div className="container mx-auto px-5 py-10">
-      <div className="flex flex-wrap justify-center">
-        {/* recorremos la lista de elementos */}
-        {listElements.map((element) => (
-          <article className="block m-1 relative p-3 rounded-lg shadow-2xl w-full overflow-hidden bg-gray-500 md:w-1/2 lg:w-1/4">
-            <div className="flex justify-between item-center">
-              <div className="flex justify-between item-center">
-                <h3 className="title-font text-lg my-2 tracking-widest text-blue-800">{element.title.toUpperCase()}</h3>
-                {(element.size) && (
-                <div className="m-2 pl-1">
-                  <span>
-                    <i className="fa-solid fa-users" />
-                  </span>
-                  +
-                  {element.size}
-                </div>
-                )}
-              </div>
-              {/* //TODO: configurar boton para editar nota si eres el creador o admin */}
-              <button className="pr-3" type="button">
-                <i className="fa-solid fa-ellipsis-vertical" />
-              </button>
-            </div>
-            <p>{element.description}</p>
-            {/* hashtag */}
-            {(element.hashtags) && (
-            <div className="pt-4">
-              {element.hashtags.map((hashtag) => (
-                <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-xs font-semibold text-gray-700 mr-2 mb-2">
-                  #
-                  {hashtag}
-                </span>
-              ))}
-            </div>
-            )}
-          </article>
-        ))}
-      </div>
-    </div>
+    <button type="button" onClick={onClick} title="Agregar" className="fixed bottom-10 right-24 z-50 text-white px-4 w-auto h-10 bg-blue-700 rounded-full hover:bg-blue-800 active:shadow-lg mouse shadow transition ease-in duration-200 focus:outline-none">
+      <svg viewBox="0 0 20 20" enableBackground="new 0 0 20 20" className="w-6 h-6 inline-block">
+        <path
+          fill="#FFFFFF"
+          d="M16,10c0,0.553-0.048,1-0.601,1H11v4.399C11,15.951,10.553,16,10,16c-0.553,0-1-0.049-1-0.601V11H4.601
+                                    C4.049,11,4,10.553,4,10c0-0.553,0.049-1,0.601-1H9V4.601C9,4.048,9.447,4,10,4c0.553,0,1,0.048,1,0.601V9h4.399
+                                    C15.952,9,16,9.447,16,10z"
+        />
+      </svg>
+      <span>Agregar</span>
+
+    </button>
   );
 }
 
 export default function Proyectos() {
   const categories = ['Proyectos', 'Ideas'];
+  const [isOpen1, setIsOpen1] = useState(false);
+  const [isOpen2, setIsOpen2] = useState(false);
+
   const listElements = [
     {
       title: 'Proyecto 1',
@@ -93,8 +73,17 @@ export default function Proyectos() {
           ))}
         </Tab.List>
         <Tab.Panels>
-          <Tab.Panel><Cards listElements={listElements} /></Tab.Panel>
-          <Tab.Panel><Cards listElements={listElements} /></Tab.Panel>
+          <Tab.Panel>
+            <CardsProyects listElements={listElements} />
+            {/* //TODO: lo del formulario no funciono intente modular asi, pero hay que intentar otra forma */}
+            <CreateModal isOpen={isOpen1} setIsOpen={setIsOpen1} title="Crear una idea para OSUC" formulario={FormProyectos} />
+            <BtnCreate onClick={() => setIsOpen1(true)} />
+          </Tab.Panel>
+          <Tab.Panel>
+            <CardsProyects listElements={listElements} />
+            <CreateModal isOpen={isOpen2} setIsOpen={setIsOpen2} title="UwU" formulario={FormIdeas} />
+            <BtnCreate onClick={() => setIsOpen2(true)} />
+          </Tab.Panel>
         </Tab.Panels>
       </Tab.Group>
     </section>
