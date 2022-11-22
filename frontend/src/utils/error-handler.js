56075b48b73da3f9c78568902d3a3c89b70e2a9a
Agustín Covarrubias <agucova@uc.cl>
2022-11-22 08:00:46 +0000

    fix: handle unknown errors in the UI

--- frontend/src/utils/error-handler.js
+++ frontend/src/utils/error-handler.js
@@ -1,6 +1,12 @@
 export default function handleError(err) {
   if (err.response) {
-    return `El servidor respondió con un error (${err.response.status}): ${err.response.data.message}.`;
+    let message = '';
+    if (err.response?.data?.message) {
+      message = err.response.data.message;
+    } else {
+      message = 'error inesperado';
+    }
+    return `El servidor respondió con un error (${err.response.status}): ${message}.`;
   } if (err.request) {
     return 'No se pudo conectar con el servidor.';
   }
